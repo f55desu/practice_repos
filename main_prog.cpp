cad450a1c5e1687394f2a16d20679af6390a5501
f55desu <67709368+f55desu@users.noreply.github.com>
2020-07-13 22:15:56 +0300

    Add files via upload

--- main_prog.cpp
+++ main_prog.cpp
@@ -5,6 +5,8 @@
 #include "geometry/Shape.h"
 #include "geometry/figures/Pyramid.h"
 #include "geometry/figures/Cube.h"
+#include "geometry/figures/Prizm.h"
+#include "geometry/figures/parallelepiped.h"
 
 using namespace std;
 
@@ -13,6 +15,8 @@ Shape* figure;
 double volume;
 double all_square;
 double brink;
+double face;
+double facet;
 
 void figure_props(const string& myFigure)
 {
@@ -20,6 +24,11 @@ void figure_props(const string& myFigure)
     cout << "\nThe Volume: " << volume;
     cout << "\nThe All Square: " << all_square;
     cout << "\nThe Brink: " << brink;
+    if (myFigure == "Parallelepiped")
+    {
+        cout << "\nThe Face: " << face;
+        cout << "\nThe Facet: " << facet;
+    }
     cout << "\nThe Peaks: ";
     for (int i = 0; i < figure->getPeaksCount(); i++)
     {
@@ -42,19 +51,22 @@ int main()
         cout << "\nChoose an option:";
 
         cout << "\n1 - Pyramid";
-        cout << "\n2 - Prism***";
-        cout << "\n3 - Parallelepiped***";
+        cout << "\n2 - Prism";
+        cout << "\n3 - Parallelepiped";
         cout << "\n4 - Cube";
         cout << "\n0 - Exit";
         cout << "\nYour choice <<< ";
         cin >> choice;
 
-        switch (choice) {
-            case 0: {
+        switch (choice) 
+        {
+            case 0: 
+            {
                 isRun = false;
                 break;
             }
-            case 1: {
+            case 1: 
+            {
                 double a, h;
                 cout << "\nEnter side <<< ";
                 cin >> a;
@@ -68,6 +80,46 @@ int main()
                 peaks = figure->find_peaks();
 
                 figure_props("Pyramid");
+                break;
+            }
+            case 2:
+            {
+                double a, b, h;
+                cout << "\nEnter side <<< ";
+                cin >> a;
+                cout << "\nEnter the other side <<< ";
+                cin >> b;
+                cout << "\nEnter height <<< ";
+                cin >> h;
+
+                figure = new Prizm(a, b, h);
+                volume = figure->find_volume();
+                all_square = figure->find_all_square();
+                brink = figure->find_brink();
+                peaks = figure->find_peaks();
+
+                figure_props("Prizm");
+                break;
+            }
+            case 3:
+            {
+                double a, b, h;
+                cout << "\nEnter side <<< ";
+                cin >> a;
+                cout << "\nEnter the other side <<< ";
+                cin >> b;
+                cout << "\nEnter height <<< ";
+                cin >> h;
+
+                figure = new parallelepiped(a, b, h);
+                volume = figure->find_volume();
+                face = figure->find_face();
+                facet = figure->find_facet();
+                brink = figure->find_brink();
+                peaks = figure->find_peaks();
+                all_square = figure->find_all_square();
+
+                figure_props("Parallelepiped");
                 break;
             }
             case 4:
@@ -85,7 +137,8 @@ int main()
                 figure_props("Cube");
                 break;
             }
-            default: {
+            default: 
+            {
                 cout << "\nAn Error has been occurred!";
                 break;
             }
